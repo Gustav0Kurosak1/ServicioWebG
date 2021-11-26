b5958a1c06da15180bd46b1e6d31e7a3351b1460
Gustav0Kurosak1 <95064301+Gustav0Kurosak1@users.noreply.github.com>
2021-11-26 00:13:45 -0600

    Update prueba1.php

--- prueba1.php
+++ prueba1.php
@@ -1,202 +1,233 @@
 <?php
-// Desactivar toda notificación de error
-error_reporting(0);
-// Notificar solamente errores de ejecución
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-define("MAX_RESULTS", 15);
-    
-     if (isset($_POST['submit']) )
-     {
-        $keyword = $_POST['keyword'];
-               
-        if (empty($keyword))
-        {
-            $response = array(
-                  "type" => "error",
-                  "message" => "Por favor ingrese la palabra clave."
-                );
-        } 
-    }
-         
+//  1 = Activo, en comentario = desactivado  ejem: //define(....
+define(CACHE,"1");
+
+require_once 'Clases/Buscador.lib.php';
+require_once 'Clases/Cache.class.php';
+
+if(defined(CACHE)){
+    $cache = new TrabajaCache;
+    $cache->CreaCache();
+}
+$q = htmlentities($_GET['q']);
+$pag = htmlentities($_GET['page']);
+$buscador = new BuscadorMp3($q,$pag);
+$buscador->web = "http://www.entra-ya.com.ar/musica/";
+$buscador->Descarga = 1;
+
+if($_GET['q']){
+    $buscadorS = $buscador->agregarBusqueda();
+    $_q = str_replace("+"," ", 
+          str_replace("-", " ",
+          $buscador->limpiar($q)));
+}
+
+if($_POST['q']){
+$qp = htmlentities($_POST['q']);
+    header("location: ".$buscador->web."buscar-".str_replace(" ","-",$qp)."/");
+    die();
+}
+
 ?>
-<!doctype html>
-<html lang="es">
-        <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="icon" href="favicon.ico">
-        <title>Descargar buscador de videos PHP YouTube API - BaulPHP</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="es" lang="es"> 
+<head>
+<title>Entra-ya | Musica - Escucha la vida</title> 
+<meta name='description' content='Busca, escucha y descarga tus mejores canciones sin tener que esperar para descargar ni escuchar sin limites'> 
+<meta name='keywords' content='musica gratis,musica descargar,descargar sin limites,no esperar,canciones,descargar,musica online, buscador de musica,buscador,buscador musica'>  
+<base href='<?=$buscador->web?>'/> 
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<style type="text/css">
+<!--
+.new-search {
+	font-family:Helvetica, Arial, sans-serif;
+	margin-bottom: 10px;
+}
+.new-search .search-body {
+	background: url(http://o2.t26.net/images/bg-search-body.png) top left repeat-x;
+	padding: 12px;
+	-moz-border-radius: 5px 5px 5px 5px;
+	-webkit-border-radius: 5px 5px 5px 5px;
+	border-radius: 5px 5px 5px 5px;
+	position: relative;
+	border: 1px solid #000;
+	z-index: 30;
+	height: 40px;
+}
+.new-search.posts .search-body { background-color: #114f7c; }
+.new-search .search-body .input-search-left,.new-search .search-body .input-search-middle,.new-search .search-body .input-search-right {
+	display: block;
+	float: left;
+	height: 42px;
+	background-image: url(http://o2.t26.net/images/input-home.png);
+	width: 5px;
+}
 
-        <!-- Bootstrap core CSS -->
-        <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-        <!-- Custom styles for this template -->
-        <link href="assets/sticky-footer-navbar.css" rel="stylesheet">
-        <style>
-.videos-data-container {
-	background: #F0F0F0;
-	border: #e0dfdf 1px solid;
-	padding: 1px;
-	border-radius: 2px;
-}
-.response {
-	padding: 10px;
-	margin-top: 10px;
-	border-radius: 2px;
-}
-.error {
-	background: #fdcdcd;
-	border: #ecc0c1 1px solid;
-}
-.success {
-	background: #c5f3c3;
-	border: #bbe6ba 1px solid;
-}
-.result-heading {
-	margin: 10px 0px;
-	padding: 10px 10px 5px 0px;
-	border-bottom: #e0dfdf 1px solid;
-}
-iframe {
-	border: 0px;
-}
-.video-tile {
-	display: inline-block;
-	margin: 10px 10px 20px 10px;
-}
-.videoDiv {
-	width: 240px;
-	height: 150px;
-	display: inline-block;
-}
-.videoTitle {
-	text-overflow: ellipsis;
-	white-space: nowrap;
-	overflow: hidden;
-}
-.videoDesc {
-	text-overflow: ellipsis;
-	white-space: nowrap;
-	overflow: hidden;
-}
-.videoInfo {
-	width: 240px;
-}
+	.new-search .search-body .input-search-left {
+		background-repeat: no-repeat;
+		background-position: 0 0;
+	}
+	
+	
+	.new-search .search-body .input-search-middle {
+		background-color:transparent;
+		background-position:0 -42px;
+		background-repeat:repeat-x;
+		border:0 none;
+		font-family:Helvetica, Arial, sans-serif;
+		font-size:18px;
+		font-weight:bold;
+		height:21px;
+		margin:0;
+		color: 999;
+		padding:10px 4px;
+		width:854px;
+		color: #999;
+	}
+	
+	.new-search .search-body .input-search-right {
+		background-repeat: no-repeat;
+		background-position: bottom right;
+	}
+	
+	.btn-search-home {
+		position:absolute;
+		z-index:10;
+		right:16px;
+		top:15px;
+		display:block;
+		width:39px;
+		height:35px;
+		background: url(http://o2.t26.net/images/btn-home-search.png) no-repeat top left;
+		
+	}
+	#centro{
+		padding:2px;
+		padding-left:6px;
+		padding-right:6px;
+		width:900px;
+		height:auto;
+	}
+	#todo {
+		position: absolute;
+		width: 1015px;
+		height: 450px;;
+	}
+	body {
+		background-image: url(http://www.entra-ya.com.ar/musica/fondo.jpg);
+	}
+	#temas{
+		padding:2px;
+		padding-left:60px;
+		padding-right:6px;
+		width:600px;
+		height:auto;
+		position: absolute;
+	}
+	#temas2{
+		padding:2px;
+		padding-left:6px;
+		padding-right:60px;
+		width:280px;
+		height:auto;
+		float:right;
+	}
+	#temas3{
+		padding: 240px 6px 2px 60px;
+		width:600px;
+		height:auto;
+	}
+	.tdentro {
+		background: url(http://o2.t26.net/images/bg-search-body.png) top left repeat-x;
+		padding: 12px;
+		-moz-border-radius: 5px 5px 5px 5px;
+		-webkit-border-radius: 5px 5px 5px 5px;
+		border-radius: 5px 5px 5px 5px;
+		position: relative;
+		border: 1px solid #000;
+		z-index: 30;
+		text-align: left;
+	}
+-->
 </style>
-        </head>
+		<script type="text/javascript"> 
+ 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17773237-2']);
+  _gaq.push(['_trackPageview']);
+ 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+ 
+</script> 
+<script type="text/javascript" src="js/core.js"></script>
+<script type="text/javascript" src="js/player.js"></script>
+<script type="text/javascript"> function fnHP(lnk){      lnk.style.behavior='url(#default#homepage)'; 	sQueryHome = lnk.isHomePage('http://imbaty.com'); 	if(!sQueryHome) lnk.setHomePage('http://google.com'); 	   return true; } </script>
 
-        <body>
-<header> 
-          <!-- Fixed navbar -->
-          <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"> <a class="navbar-brand" href="#">BaulPHP</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-              <ul class="navbar-nav mr-auto">
-        <li class="nav-item active"> <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a> </li>
-      </ul>
-              <form class="form-inline mt-2 mt-md-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busqueda</button>
-      </form>
-            </div>
-  </nav>
-        </header>
-
-<!-- Begin page content -->
-
-<div class="container">
-          <h3 class="mt-5">Buscador de videos PHP YouTube </h3>
-          <hr>
-          <div class="row">
-    <div class="col-12 col-md-12">
-    <!-- Contenido -->
-    
-    <form id="keywordForm" method="post" action="">
-              <div class="form-row align-items-center">
-        <div class="col-auto"> Buscar palabra clave: </div>
-        <div class="form-row align-items-center">
-                  <div class="col-auto">
-            <input class="form-control mb-2" type="search" id="keyword" name="keyword"  placeholder="Ingrese palabra a buscar">
-          </div>
-                  <div class="col-auto">
-            <input class="btn btn-primary mb-2" type="submit" name="submit" value="Busqueda">
-          </div>
-                </div>
-      </div>
-              </div>
-            </form>
-    <?php if(!empty($response)) { ?>
-    <div class="response <?php echo $response["type"]; ?>"> <?php echo $response["message"]; ?> </div>
-    <?php }?>
-    <?php
-            if (isset($_POST['submit']) )
-            {
-                                         
-              if (!empty($keyword))
-              {
-			  $apikey = 'SU API KEY';
-              $googleApiUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='.$keyword.'&maxResults='.MAX_RESULTS.'&key='.$apikey;
-
-                $ch = curl_init();
-
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-                curl_setopt($ch, CURLOPT_VERBOSE, 0);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                $response = curl_exec($ch);
-
-                curl_close($ch);
-                $data = json_decode($response);
-                $value = json_decode(json_encode($data), true);
-            ?>
-    <div class="result-heading">Acerca de <?php echo MAX_RESULTS; ?> Resultados</div>
-    <div class="videos-data-container" id="SearchResultsDiv">
-              <?php
-                for ($i = 0; $i < MAX_RESULTS; $i++) {
-                    $videoId = $value['items'][$i]['id']['videoId'];
-                    $title = $value['items'][$i]['snippet']['title'];
-                    $description = $value['items'][$i]['snippet']['description'];
-                    ?>
-              <div class="video-tile">
-        <div  class="videoDiv">
-                  <iframe id="iframe" style="width:100%;height:100%" src="//www.youtube.com/embed/<?php echo $videoId; ?>" data-autoplay-src="//www.youtube.com/embed/<?php echo $videoId; ?>?autoplay=1"></iframe>
-                </div>
-        <div class="videoInfo">
-                  <div class="videoTitle"><b><?php echo $title; ?></b></div>
-                  <div class="videoDesc"><?php echo $description; ?></div>
-                </div>
-      </div>
-              <?php 
-                    }
-                } 
-           
-            }
-            ?>
-              
-              <!-- Fin Contenido --> 
-            </div>
-  </div>
-          <!-- Fin row --> 
-          
-        </div>
-<!-- Fin container -->
-<footer class="footer">
-          <div class="container"> <span class="text-muted">
-            <p>Códigos <a href="https://www.baulphp.com/" target="_blank">BaulPHP</a></p>
-            </span> </div>
-        </footer>
-<script src="assets/jquery-1.12.4-jquery.min.js"></script> 
-<script src="assets/jquery.validate.min.js"></script> 
-<script src="assets/ValidarRegistro.js"></script> 
-<!-- Bootstrap core JavaScript
-    ================================================== --> 
-<!-- Placed at the end of the document so the pages load faster --> 
-
-<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script> 
-<script src="assets/js/vendor/popper.min.js"></script> 
-<script src="dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="todo">
+<center>
+<div id="centro">
+<div class="new-search posts"><!-- buscador --> 
+	<div class="search-body clearfix"> 
+		<form name="search" action="index.php" method="post"> 
+			<div class="input-search-left"></div> 
+			<input type="hidden" name="procesar" value="1" />
+			<input class="input-search-middle" name="q" type="text" value="<?=$_q?>" /> 
+			<div class="input-search-right"></div> 
+			<input type="submit"  class="btn-search-home" value="" />  
+					</form> 
+	</div> 
+</div></div></center>
+<center>
+<div id="temas">
+<div class="tdentro">
+<div id="lista">
+<?php
+                    
+                        if($buscadorS == false){
+                            echo "No encontramos resultados para su busqueda: ".$_q;
+                        }else{
+                            for($i=0;$i<count($buscadorS[0]);$i++){
+                                echo $buscador->nuevoSong($buscadorS[1][$i],
+                                                          $buscadorS[2][$i],
+                                                          $buscadorS[3][$i]);
+                            }
+                        }
+                        
+?></div>
+</div></div>
+<div id="temas2"><div class="tdentro">
+<?
+if($_GET['q']){
+?>
+<div id="player"><center><script type="text/javascript"><!--
+google_ad_client = "ca-pub-7196350699220854";
+/* musica1 */
+google_ad_slot = "2214468750";
+google_ad_width = 250;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></center></div>
+<?php } ?>
+</div></div></center>
+<div id="temas3">
+<div class="tdentro">
+<?
+require_once "Clases/Paginated.php";
+require_once "Clases/DoubleBarLayout.php";
+$paginacion = new Paginated($buscador->totalResultados(), 10, $pag);
+$paginacion->setLayout(new DoubleBarLayout($_q));
+echo $paginacion->fetchPagedNavigation();
+?>
+</div></div></div>
 </body>
 </html>
+<? (defined(CACHE)?$cache->CierraCache():''); ?>
